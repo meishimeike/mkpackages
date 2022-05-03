432ac3d70d413b3b67614d0c41a676f31a4d9f07
meishimeike <peter.km.zhang@gmail.com>
2022-05-03 22:06:03 +0800

    Add chinadns

--- luci-app-mwan3helper/luasrc/model/cbi/mwan3helper/client.lua
+++ luci-app-mwan3helper/luasrc/model/cbi/mwan3helper/client.lua
@@ -12,17 +12,12 @@ o = s:option(Flag, "enabled", translate("Enable GFW IPSet"))
 o.rmempty = false
 o.description = translate("启用 DNS 防污染并打开GFW IPSet（与SSR的GFW列表模式不能同时开启）")
 
-o = s:option(ListValue, "dns", translate("Anti-pollution DNS Server"))
+o = s:option(Value, "dns", translate("Anti-pollution DNS Server"))
 o:value("208.67.222.222:443", translate("OpenDNS (208.67.222.222:443)"))
 o:value("208.67.220.220:443", translate("OpenDNS (208.67.220.220:443)"))
 o:value("8.8.4.4:53", translate("Google Public DNS (8.8.4.4:53)"))
 o:value("8.8.8.8:53", translate("Google Public DNS (8.8.8.8:53)"))
-o:value("209.244.0.3:53", translate("Level 3 Public DNS (209.244.0.3:53)"))
-o:value("209.244.0.4:53", translate("Level 3 Public DNS (209.244.0.4:53)"))
-o:value("4.2.2.1:53", translate("Level 3 Public DNS (4.2.2.1:53)"))
-o:value("4.2.2.2:53", translate("Level 3 Public DNS (4.2.2.2:53)"))
-o:value("4.2.2.3:53", translate("Level 3 Public DNS (4.2.2.3:53)"))
-o:value("4.2.2.4:53", translate("Level 3 Public DNS (4.2.2.4:53)"))
 o:value("1.1.1.1:53", translate("Cloudflare DNS (1.1.1.1:53)"))
+o.default="208.67.222.222:443"
 
 return m
